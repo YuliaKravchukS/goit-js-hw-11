49866feb943222ae9dfc3d67d0515180b61f63a6
YuliaKravchukS <ljuljek554@gmail.com>
2024-01-31 15:44:14 +0200

    css+html

---
+++ src/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const refs = {
+    formEl: document.querySelector('.form'),
+    galleryEl: document.querySelector('.gallery'),
+}
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+};
+
+function getUrl() {
+    const BASE_URL: '',
+    const POINT: '',
+    const PARAMS: '',
+    
+}
