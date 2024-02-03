4173f7300b0b4e8bde5e199f94f9416939777b9e
YuliaKravchukS <ljuljek554@gmail.com>
2024-02-03 14:20:04 +0200

    cddc

--- src/main.js
+++ src/main.js
@@ -1,25 +1,65 @@
 'use strict'
 
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 const refs = {
     formEl: document.querySelector('.form'),
     galleryEl: document.querySelector('.gallery'),
+    
 }
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+    },
+]
+ refs.formEl.addEventListener('submit', onFormSubmit);
 
-refs.formEl.addEventListener('submit', onFormSubmit);
+//     const markup = images.map(({preview, original, description}) => {
+//         return `<li class="gallery-item2">
+//  <div>
+//      <div class="gallery-item">
+//           <a class="gallery-link" href="${original}">
+//             <img class="gallery-image" src="${preview}" alt="${description}" />
+//           </a>
+//      </div>
+//  </div>
+// </li>`}).join('');
+//     refs.galleryEl.insertAdjacentHTML('beforeend',markup);
+
+
 
 function onFormSubmit(e) {
     e.preventDefault();
+     refs.galleryEl.remove;
     const value = e.target.elements.valueGallery.value;
-    // getUrl(value)
+   
     getUrl(value).then(data => {
+        if (data.length === 0) {
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+    position: 'center',
+})
+        }
         renderImages(data);
-  });
+            gallery.open();
+    console.log(gallery);
+    });
+    
     e.target.reset();
 };
 
@@ -35,34 +75,41 @@ function getUrl(clientValue) {
     });
     const url = `${BASE_URL}${ENDPOINT}?${searchParams}`;
     
-    return fetch(url).then(data => data.json()).then(data => data.hits); ;
+    return fetch(url)
+        .then(data => {
+            if (!data.ok) {
+                throw new Error(data.status);
+            }
+            return data.json()
+        })
+        .then(data => data.hits);
+
 }
 
 
 
 function imageTemplate(img) {
     const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = img;
-    console.log(webformatURL);
+    
     return `<li>
     <div class="card">
     <div class="img-container">
-    <img
-    src=${webformatURL}
-    alt= ${ tags }
-    width="360"
-    height="200"
-    />
+   <a href="${largeImageURL}">
+                    <img src="${webformatURL}" alt="${tags}" />
+                </a>
     </div>
     <div class="img-comments">
-    <p>Likes ${ likes }</p>
-    <p>Views ${ views }</p>
-    <p>Comments ${ comments }</p>
-    <p>Downloads ${downloads}</p>
+    <p class="describe">Likes ${ likes }</p>
+    <p class="describe">Views ${ views }</p>
+    <p class="describe">Comments ${ comments }</p>
+    <p class="describe">Downloads ${downloads}</p>
     </div>
     </div>
     </li>
     `;
 }
+
+
 function imagesTemplate(images) {
     return images.map(imageTemplate).join('');
 }
@@ -70,3 +117,6 @@ function renderImages(images) {
     const markup = imagesTemplate(images) ;
     refs.galleryEl.innerHTML = markup;
 };
+     let gallery = new SimpleLightbox('.img-container a');
+
+gallery.on('show.simplelightbox', function () {});
